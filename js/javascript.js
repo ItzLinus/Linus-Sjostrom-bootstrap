0a8f477722abbf2de6d1a5ad028dfbe2e38af091
Linus <70134500+ItzLinus@users.noreply.github.com>
2021-11-25 10:13:28 +0100

    §§

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,56 @@
-var indicator = document.querySelector('.indicator');
-var items = document.querySelectorAll('.nav-item');
-function handleIndicator(eL) {
-    items.forEach(function(eL){
-        item.ClassList.remove('active');
-        item.removeAttribute('style');
-    });
-
-    indicator.style.width = "".concat(el.offsetWidth, "px");
-    indicator.style.left = "".concat(el.offsetLeft, "px");
-    indicator.style.backgroundColor = el.getAttribute('active-color');
-    el.ClassList.add('active');
-    el.style.color = el.Attributes('active-colors');
+function test(){
+	var tabsNewAnim = $('#navbarSupportedContent');
+	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+	var activeItemNewAnim = tabsNewAnim.find('.active');
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		"top":itemPosNewAnimTop.top + "px", 
+		"left":itemPosNewAnimLeft.left + "px",
+		"height": activeWidthNewAnimHeight + "px",
+		"width": activeWidthNewAnimWidth + "px"
+	});
+	$("#navbarSupportedContent").on("click","li",function(e){
+		$('#navbarSupportedContent ul li').removeClass("active");
+		$(this).addClass('active');
+		var activeWidthNewAnimHeight = $(this).innerHeight();
+		var activeWidthNewAnimWidth = $(this).innerWidth();
+		var itemPosNewAnimTop = $(this).position();
+		var itemPosNewAnimLeft = $(this).position();
+		$(".hori-selector").css({
+			"top":itemPosNewAnimTop.top + "px", 
+			"left":itemPosNewAnimLeft.left + "px",
+			"height": activeWidthNewAnimHeight + "px",
+			"width": activeWidthNewAnimWidth + "px"
+		});
+	});
 }
+$(document).ready(function(){
+	setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+	setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function(){ test(); });
+});
 
-items.forEach(function (item, index){
-    item.addEventListener('click', function(e){
-        handleIndicator(e.target);
-    });
-    item.ClassList.container('active') && handleIndicator(item);
+
+
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function($){
+	// Get current path and find target link
+	var path = window.location.pathname.split("/").pop();
+
+	// Account for home page with empty path
+	if ( path == '' ) {
+		path = 'index.html';
+	}
+
+	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+	// Add active class to target link
+	target.parent().addClass('active');
 });
